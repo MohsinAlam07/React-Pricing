0c6b3267d8cacc861bd821ecdcb8f3d5c4732fcf
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-03-09 12:16:10 +0600

    toggle navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,51 +1,72 @@
-import React from 'react';
+import React, { useState } from "react";
+import Link from "./Link";
+import { Menu, X } from "lucide-react";
 const navData = [
   {
     id: 1,
     name: "Home",
-    path: "/"
+    path: "/",
   },
   {
     id: 2,
     name: "About",
-    path: "/about"
+    path: "/about",
   },
   {
     id: 3,
     name: "Services",
-    path: "/services"
+    path: "/services",
   },
   {
     id: 4,
     name: "Blog",
-    path: "/blog"
+    path: "/blog",
   },
   {
     id: 5,
     name: "Contact",
-    path: "/contact"
-  }
+    path: "/contact",
+  },
 ];
 
+const Links = navData.map((route) => (
+  <Link key={route.id} route={route}></Link>
+));
 
 const Navbar = () => {
-    return (
-        <nav>
+  const [open, setOpen] = useState(false);
 
-            <ul className='flex'>
+  return (
+    <nav className="flex justify-between mx-10">
+      <span className="flex" onClick={() => setOpen(!open)}>
+        {open ? (
+          <X className="md:hidden"></X>
+        ) : (
+          <Menu className="md:hidden"></Menu>
+        )}
+        <ul className="md:hidden">
+          {Links}
+        </ul>
+
+        <h3 className="ml-5">My Navbar</h3>
+      </span>
+
+      <ul className="md:flex hidden">{Links}</ul>
+      {/* <ul className='flex'>
                 {
                     navData.map(route=> <li className='mr-10 '> <a href={route.path}>{route.name}</a> </li>)
                 }
-            </ul>
-            {/* <ul className='flex'>
+            </ul> */}
+      {/* <ul className='flex'>
                 <li className='mr-10'><a href="/">Home </a></li>
                 <li className='mr-10'><a href="/about">About</a></li>
                 <li className='mr-10'><a href="/blog">Blog</a></li>
 
 
             </ul> */}
-        </nav>
-    );
+      <button>Sign In</button>
+    </nav>
+  );
 };
 
 export default Navbar;
